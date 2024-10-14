d4370e47c771ac1995905343b3c2d42a7d8b279c
cacalucuwww <salsabrina33@gmail.com>
2024-10-14 17:10:30 +0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,47 @@
+<?php
+    session_start();
+	if($_SESSION['status_login'] != true){
+		echo '<script>window.location="login.php"</script>';
+    }
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>WEB GALERI FOTO HEWAN</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <!-- header -->
+    <header>
+        <div class="container">
+        <h1><a href="dashboard_admin.php">WEB GALERI FOTO HEWAN</a></h1>
+        <ul>
+           <li><a href="profil.php">Profil</a></li>
+           <li><a href="data-image.php">Data Foto</a></li>
+           <li><a href="Keluar.php">Keluar</a></li>
+        </ul>
+        </div>
+    </header>
+    
+    <!-- content -->
+    <div class="section">
+        <div class="container">
+            <h3>Dashboard</h3>
+            <div class="box">
+                <h4>Selamat Datang <?php echo $_SESSION['a_global']->nama ?> di dashboard</h4>
+            </div>
+        </div>
+    </div>
+
+    
+    <!-- footer -->
+    <footer>
+        <div class="container">
+            <small>Copyright &copy; 2024 - WEB GALERI FOTO HEWAN.</small>
+        </div>
+    </footer>
+</body>
+</html>
